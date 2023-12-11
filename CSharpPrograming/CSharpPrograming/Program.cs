49f9b689006767edabe0e465b2125d501cff9619
trueghoulzxc <ivan1vanov73@yandex.ru>
2023-12-11 22:23:01 +0400

    t1

---
+++ CSharpPrograming/CSharpPrograming/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpPrograming;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        TestDates();
+
+        //явно вызываем сборку мусора для демонстрации работы деструктора
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+
+    static void TestDates()
+    {
+        MyDate date1 = new();
+        Console.Write("date1: ");
+        date1.Print();
+
+        MyDate date2 = new(11, 12, 2023);
+        Console.Write("date2: ");
+        date2.PrintWithMonthName();
+
+        MyDate date3 = new(date2);
+        Console.Write("date3: ");
+        date3.Print();
+
+        date2.Day = 31;
+        Console.WriteLine(date2.Day);
+
+        date2.Day = 32;
+        Console.WriteLine(date2.Day);
+    }
+}
