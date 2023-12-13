e457836c31c131e6000b01ee7b43a925f070f893
trueghoulzxc <ivan1vanov73@yandex.ru>
2023-12-13 19:20:01 +0400

    t5

---
+++ CSharpPrograming/CSharpPrograming/Cqueue.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace CSharpPrograming;
+
+internal class Cqueue : IEnumerable
+{
+    private int?[] _items = new int?[16];
+    private int _lastPosition = 0;
+
+    public Cqueue()
+    {
+        Console.WriteLine("Вызван конструктор без параметров");
+    }
+
+    public Cqueue(IEnumerable<int> items)
+    {
+        Console.WriteLine("Вызван конструктор с параметрами");
+        
+        foreach (var item in items)
+        {
+            this.Add(item);
+        }
+    }
+
+    public void Add(int newItem)
+    {
+        if (_lastPosition + 1 >= _items.Length)
+        {
+            _items = IncreaseCapacity();
+        }
+
+        _items[_lastPosition] = newItem;
+        _lastPosition++;
+    }
+
+    private int?[] IncreaseCapacity()
+    {
+        int?[] newItems = new int?[_items.Length * 2];
+
+        int startIdx = GetFirstNotNullIndex();
+        int endIdx = GetLastNotNullIndex();
+
+        Array.Copy(_items, startIdx, newItems, 0, endIdx - startIdx);
+        return newItems;
+    }
+
+    public int? Get() 
+    {
+        int notNullIdx = GetFirstNotNullIndex();
+        
+        if (notNullIdx == -1)
+            return null;
+
+        int? res = _items[notNullIdx];
+        _items[notNullIdx] = null;
+        return res;
+    }
+
+    private int GetFirstNotNullIndex()
+    {
+        for (int i = 0; i < _items.Length; i++)
+        {
+            if (_items[i] != null)
+                return i;
+        }
+
+        return -1;
+    }
+    private int GetLastNotNullIndex()
+    {
+        for (int i = _items.Length - 1; i >= 0; i--)
+        {
+            if (_items[i] != null)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return _items.Where(x => x != null).GetEnumerator();
+    }
+
+    public static Cqueue operator +(Cqueue queue, int newItem)
+    {
+        queue.Add(newItem);
+        return queue;
+    }
+
+    public static int? operator -(Cqueue queue)
+    {
+        return queue.Get();
+    }
+
+    /// <summary>
+    /// копирование одной очереди в другую с сортировкой в убывающем порядке
+    /// </summary>
+    public static Cqueue operator <(Cqueue queue1, Cqueue queue2)
+    {
+        foreach (var item in queue2._items.OrderByDescending(x => x))
+        {
+            if (item != null)
+                queue1.Add((int)item);
+        }
+
+        return queue1;
+    }
+
+    public static Cqueue operator >(Cqueue queue1, Cqueue queue2)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Проверка на пустоту очереди. Возвращает true если очередь пустая, иначе false
+    /// </summary>
+    public static explicit operator bool(Cqueue queue)
+    {
+        return queue.GetFirstNotNullIndex() == -1;
+    }
+
+    /// <summary>
+    /// Мощность (количество уникальных элементов)
+    /// </summary>
+    public static explicit operator int(Cqueue queue)
+    {
+        return queue._items.Where(x => x != null).Distinct().Count();
+    }
+
+    ~Cqueue()
+    {
+        Console.WriteLine("Вызван деструктор");
+    }
+}
---
+++ CSharpPrograming/CSharpPrograming/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace CSharpPrograming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cqueue q1 = new Cqueue([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            Cqueue q2 = new Cqueue([20, 21, 22, 27, 26, 25, 23, 22, 21]);
+
+            PrintCqueue(q1);
+
+
+            Console.WriteLine("Добавление + 1");
+            q1 = q1 + 1;
+            PrintCqueue(q1);
+
+            Console.WriteLine("Получение элемента");
+            int? item = - q1;
+            Console.WriteLine("Получен элемент " + item);
+            PrintCqueue(q1);
+
+            Console.WriteLine("Копирование одной очереди в другую с сортировкой в убывающем порядке");
+            q1 = q1 < q2;
+            PrintCqueue(q1);
+
+            Console.WriteLine("Проверка пустая ли очередь: " + (bool)q1);
+            Console.WriteLine("Мощность: " + (int)q1);
+        }
+
+        static void PrintCqueue(Cqueue queue)
+        {
+            Console.Write("Элементы очереди ");
+            foreach (var item in queue)
+            {
+                Console.Write(item + "; ");
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
